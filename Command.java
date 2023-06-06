cd2d3ec7144d3c089005944da7ab43de4fa4bd80
nallameghana <112631279+nallameghana@users.noreply.github.com>
2023-06-06 12:09:29 +0530

    Add files via upload

---
+++ Command.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Command
+{
+	public static void main(String args[])
+	{
+		String str = args[0];
+		System.out.print("String : ");
+		int b = Integer.parseInt(args[1]);
+		System.out.println(str);
+		System.out.print("Number : ");
+		System.out.print(b);
+
+	}
+}
