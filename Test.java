07825d4660344a663845180e9a75f29987bc70a8
nallameghana <112631279+nallameghana@users.noreply.github.com>
2023-06-06 12:10:23 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Test
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+                System.out.println("Enter number1 : ");
+		int a = s.nextInt();
+		System.out.println("Enter number2 : ");
+		int b = s.nextInt();
+		int c = a + b;
+		System.out.println(c);
+	}
+}
