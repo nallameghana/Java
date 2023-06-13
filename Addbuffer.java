61e5c79fc214834a441730df96fe2c4a8019ec18
nallameghana <112631279+nallameghana@users.noreply.github.com>
2023-06-13 07:39:09 +0530

    Update Addbuffer.java

--- Addbuffer.java
+++ Addbuffer.java
@@ -4,10 +4,10 @@ class Addbuffer
 	public static void main(String args[]) throws IOException
 	{
 
-		InputStreamReader ir = InputStreamReader(System.in);
-		BufferReader b = new BufferReader(ir);
+		InputStreamReader ir = new InputStreamReader(System.in);
+		BufferedReader b = new BufferedReader(ir);
 		System.out.println("Enter no1 : ");
-		int a = Integer.passInt(b.readLine());
+		int a = Integer.parseInt(b.readLine());
 		System.out.println("Enter no2 : ");
 		int c = Integer.parseInt(b.readLine());
 		System.out.print("sum : ");
