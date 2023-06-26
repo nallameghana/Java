6b601dd932191ad56829f7c5dd8c590c1db18afd
nallameghana <112631279+nallameghana@users.noreply.github.com>
2023-06-26 18:46:29 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	     Scanner s = new Scanner(System.in);
+	     System.out.println("Enter a given number : ");
+	     int n = s.nextInt();
+	     System.out.print("The entered number is : " + n);
+	}
+}
