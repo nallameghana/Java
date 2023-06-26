7042c4f7c4272c8a1bcda0f1f99723c63c2ddca7
nallameghana <112631279+nallameghana@users.noreply.github.com>
2023-06-26 23:27:55 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter 2 floating point nos : ");
+		float a = s.nextFloat();
+		float b = s.nextFloat();
+		System.out.println("Product of 2 given nos is : " + a*b);
+		
+	}
+}
